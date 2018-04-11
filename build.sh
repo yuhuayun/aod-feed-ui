ae76d46d63977a01b84537ae3d61162a544ac25c
yuhuayun <3246068556@qq.com>
2018-04-11 15:13:06 +0800

    2018

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 docker build -t 192.168.1.201:5000/aod-feed-ui:v${BUILD_NUMBER}.0 .
 docker push 192.168.1.201:5000/aod-feed-ui:v${BUILD_NUMBER}.0
 cd src
+#chmod +x rancher-compose
 #sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 #sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
-./src/rancher-compose --url http://192.168.1.201:8080 --access-key 994E36B65FE1614CA2CD --secret-key zGDwi6rXkmFvEfbPE6JuT3X92PijfY4CE8CpHU56 -p aod-feed-ui up -d
+./rancher-compose --url http://192.168.1.201:8080 --access-key 994E36B65FE1614CA2CD --secret-key zGDwi6rXkmFvEfbPE6JuT3X92PijfY4CE8CpHU56 -p aod-feed-ui up -d
