f82662f5197dc7ab507b570a5b83d4e239557207
yuhuayun <3246068556@qq.com>
2018-04-11 17:53:18 +0800

    021

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-${BUILD_NUMBER}
+echo ${BUILD_NUMBER}
 docker build -t 192.168.1.201:5000/aod-feed-ui:v${BUILD_NUMBER} .
 #docker push 192.168.1.201:5000/aod-feed-ui:v${BUILD_NUMBER}
 #cd src
